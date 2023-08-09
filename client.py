fd91737b04578b4b19dd30b062f41836143f17e3
Pranav P <104482770+pranavxp@users.noreply.github.com>
2023-08-09 16:11:49 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
-SERVER_IP = '172.16.51.127'
+SERVER_IP = 'IP of the receiver'
 PORT_NUMBER = 5000
 SIZE = 1024
 print("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
